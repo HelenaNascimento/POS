c9334aaa98293eb9211c929ea0acd5ff7608ebb8
Helena Nascimento <hnasc.1989@gmail.com>
2024-06-29 09:04:58 -0300

    'aula 02'

--- ETL/exe_01.py
+++ ETL/exe_01.py
@@ -3,9 +3,11 @@ import pandas as pd
 #Seleção de conjunto de dados
 #https://dados.gov.br/dados/conjuntos-dados/configuracoes-das-filas-de-analise-da-anvisa
 
-def load_data(df):
-    df = pd.read_csv("TL\FILA_ANALISE_ASSUNTO_SITUACAO.csv")
+def load_data():
+    df = pd.read_csv("/home/technical/WorkSpace/POS/ETL/FILA_ANALISE_ASSUNTO_SITUACAO.csv")
     return df
+
+df = load_data()
 
 pd.DataFrame(df)
 
